432d5807ae490d5c0563516b3fe4fb74d4c808f8
Neyl JABER <njaber@e1r4p7.42.fr>
2018-08-19 15:36:52 +0200

    Ray casting working

--- src/display.c
+++ src/display.c
@@ -6,37 +6,48 @@
 /*   By: njaber <neyl.jaber@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/16 05:31:19 by njaber            #+#    #+#             */
-/*   Updated: 2018/08/19 11:04:41 by njaber           ###   ########.fr       */
+/*   Updated: 2018/08/19 15:35:57 by njaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
 
-static void		paint_col(t_env *env, int col, unsigned int color)
+#define SIZE 20.
+
+static void		paint_col(t_env *env, int col, unsigned int color, t_scal dist)
 {
 	int		i;
 
 	i = -1;
 	while (++i < env->win.size.v[1])
-		img_px(&env->win.img, color, ivec(col, i));
+	{
+		if (fabs((t_scal)(i - env->win.size.v[1] / 2.) /
+				(env->win.size.v[0] / 2)) * dist > SIZE / 2)
+			img_px(&env->win.img, 0xAAAAAA, ivec(col, i));
+		else
+			img_px(&env->win.img, color, ivec(col, i));
+	}
 }
 
 static void		cast(t_env *env, int col)
 {
-	t_vec2	dir;
-	t_vec2	v;
-	t_vec2	comp;
+	t_vec2			dir;
+	t_vec2			v;
+	t_vec2			comp;
 
-	(void)col;
 	dir = m2dotv2(env->cam_mat, vec2((t_scal)(col - env->win.size.v[0] / 2) /
 				(env->win.size.v[0] / 2), 1.));
+	dir = vec2(dir.v[0] / sqrt(dir.v[0] * dir.v[0] + dir.v[1] * dir.v[1]),
+		dir.v[1] / sqrt(dir.v[0] * dir.v[0] + dir.v[1] * dir.v[1]));
 	v = vec2(env->pos.v[0], env->pos.v[2]);
-	comp = vec2(fabs(fmod(fmod(copysign(v.v[0], -dir.v[0]), 100) + 100, 100) / dir.v[0]),
-			fabs(fmod(fmod(copysign(v.v[1], -dir.v[1]), 100) + 100, 100) / dir.v[1]));
-	if (comp.v[0] > comp.v[1])
-		paint_col(env, col, 0xFFAA00);
+	comp.v[0] = dir.v[0] > 0 ? (ceil(v.v[0] / SIZE) * SIZE - v.v[0]) / dir.v[0]
+		: (floor(v.v[0] / SIZE) * SIZE - v.v[0]) / dir.v[0];
+	comp.v[1] = dir.v[1] > 0 ? (ceil(v.v[1] / SIZE) * SIZE - v.v[1]) / dir.v[1]
+		: (floor(v.v[1] / SIZE) * SIZE - v.v[1]) / dir.v[1];
+	if (comp.v[0] < comp.v[1])
+		paint_col(env, col, 0xFFAA00, comp.v[0]);
 	else
-		paint_col(env, col, 0x00AAFF);
+		paint_col(env, col, 0x0033FF, comp.v[1]);
 }
 
 void			display(t_env *env)
